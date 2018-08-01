37faad6728212ca59eff781c84fded0eed807ba1
Alena <i.am.am.629@gmail.com>
2018-08-01 17:11:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newa = []
+  for (var m = 0; m < musicians.length; m++) {
+    newa.push(`${musicians[m]} plays ${instruments[m]}`)
+  }
+  return newa
+}
